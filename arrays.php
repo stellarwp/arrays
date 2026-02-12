b7ebf47d45fcb5cbefd20bb79f30eac57c891bd2
Estevão Costa <estevao90@gmail.com>
2026-02-12 16:48:23 -0300

    Fix PHP 8.4 compatibility issues

--- arrays.php
+++ arrays.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Arrays
  * Description: Arrays library with a plugin bootstrap file for automated testing.
- * Version: 1.0.0
+ * Version: 1.3.4
  * Author: StellarWP
  * Author URI: https://stellarwp.com
  */
